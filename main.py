735675b97bcc9c690cc65037c6fab0c566b2fdb1
Buda Gabriel-Christian <79841250+budagabrielchristian@users.noreply.github.com>
2021-04-14 17:39:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 def increase_price(fruit,price):
-  list_of_fruits[fruit]=list_of_fruits[fruit]+price
+  fruits[fruit]=list_of_fruits[fruit]+price
 
 def set_price_of_fruit(fruit,price):
-    list_of_fruits[fruit]=price
+    fruits[fruit]=price
     
 def print_data(fruit):
   try:
-    print("Price per kilogram ["+str(fruit)+']:'+str(list_of_fruits[fruit])+'$')
+    print("Price per kilogram ["+str(fruit)+']:'+str(fruits[fruit])+'$')
   except:
     print(str(fruit)+" is not in the list!")
 
 def add_fruit(fruit,price):
-    list_of_fruits[fruit]=price
+    fruits[fruit]=price
 
 def print_fruits():
-  for fruit in list_of_fruits:
-    print("1 kilogram x "+str(fruit)+" costs "+str(list_of_fruits[fruit])+'$')
+  for fruit in fruits:
+    print("1 kilogram x "+str(fruit)+" costs "+str(fruits[fruit])+'$')
 
-list_of_fruits = {
+fruits = {
   "Orange":7,
   "Tomato":20,
   "Strawberry":15,
